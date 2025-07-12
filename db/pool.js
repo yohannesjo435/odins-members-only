ca66007713f82b73afcef296fcc25946c5dac7ad
yohannes <yohannesgoat@gmail.com>
2025-07-12 11:32:55 -0300

    removing process.env.DATABASE_URL for the connection string

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,10 @@
-require("dotenv").config()
 const {Pool} = require("pg")
+require("dotenv").config()
 
 console.log("Connecting to DB:", process.env.DATABASE_URL)
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: "postgresql://members_adus_user:kMibDjmzwBO7Mlbs3b4rtENbE8rN4oXW@dpg-d1p5igqdbo4c7383eb30-a/members_adus",
   ssl: { rejectUnauthorized: false }
 });
 module.exports = pool
