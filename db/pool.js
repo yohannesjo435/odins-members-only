758a687fd9db80ce1f7622cd33103a2cb9d101b0
yohannes <yohannesgoat@gmail.com>
2025-07-12 15:35:11 -0300

    changed connectionString to get DATABASE_URL from .env

--- db/pool.js
+++ db/pool.js
@@ -4,7 +4,7 @@ require("dotenv").config()
 console.log("Connecting to DB:", process.env.DATABASE_URL)
 
 const pool = new Pool({
-  connectionString: "postgresql://members_adus_user:kMibDjmzwBO7Mlbs3b4rtENbE8rN4oXW@dpg-d1p5igqdbo4c7383eb30-a/members_adus",
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 module.exports = pool
